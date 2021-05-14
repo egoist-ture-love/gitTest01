f168b5cf5ab98e0e5a280e5b8af036baf0718aa5
egoist <2469930169@qq.com>
2021-05-14 11:14:29 +0800

    第一次提交

---
+++ test01/src/main/java/ltd/egoist/cotroller/UserController.java
@@ -0,0 +1,13 @@
+package ltd.egoist.cotroller;
+
+/**
+ * @Classname UserController
+ * @Description TODO
+ * @Date 2021/5/14 11:12
+ * @Created by Lenovo
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("第一次");
+    }
+}
