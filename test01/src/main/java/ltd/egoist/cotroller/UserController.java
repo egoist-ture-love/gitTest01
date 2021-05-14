42cf1748eedffbc20a4b105a75fda5087ff227f2
egoist <2469930169@qq.com>
2021-05-14 11:22:36 +0800

    热修分支第一次提交

--- test01/src/main/java/ltd/egoist/cotroller/UserController.java
+++ test01/src/main/java/ltd/egoist/cotroller/UserController.java
@@ -11,5 +11,6 @@ public class UserController {
         System.out.println("第一次");
         System.out.println("第二次");
         System.out.println("第三次");
+        System.out.println("热修分支第一次合并");
     }
 }
