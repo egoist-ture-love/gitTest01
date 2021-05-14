8389e1a34b6e6e3392a9ab4afdc076f7f13f3ba7
egoist <2469930169@qq.com>
2021-05-14 11:17:32 +0800

    第二次提交

--- test01/src/main/java/ltd/egoist/cotroller/UserController.java
+++ test01/src/main/java/ltd/egoist/cotroller/UserController.java
@@ -9,5 +9,6 @@ package ltd.egoist.cotroller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("第一次");
+        System.out.println("第二次");
     }
 }
